3fe712ca9a149b428256334b99572f7a0188e848
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2021-02-25 06:55:04 -0300

    feat: create a screen page ListHero

--- src/pages/ListHero/index.tsx
+++ src/pages/ListHero/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import Input from '../../components/Input';
+import { BodyList, ContainerText, HeaderList, MarkerUnderline, SubTitle, Title, TitleList } from './styles';
 
 // import { Container } from './styles';
 
 const ListHero: React.FC = () => {
-  return (<View>
-    <Text >
-      List
-</Text>
+
+
+  const [name, setName] = useState('');
+
+  return (<View style={{flex:1}}>
+    <HeaderList>
+      <ContainerText>
+        <Title>BUSCA MARVEL</Title>
+        <SubTitle>TESTE FRONT-END</SubTitle>
+      </ContainerText>
+      <MarkerUnderline />
+      <Input
+        label="Nome do Personagem"
+        value={name}
+        onChange={setName}
+      />
+    </HeaderList>
+    <BodyList >
+      <TitleList>Nome</TitleList>
+    </BodyList>
   </View>);
 }
 
---
+++ src/pages/ListHero/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+
+export const HeaderList = styled.View`
+    padding:24px;
+`;
+
+export const ContainerText = styled.View`
+    flex-direction:row; 
+`;
+
+export const MarkerUnderline = styled.View`
+    width:54px;
+    height:4px;
+    background-color:#D42026;  
+`;
+
+export const Title = styled.Text`
+    font-family: Roboto-Black;
+    font-size:16px;
+    color:#D42026;  
+`;
+
+export const SubTitle = styled.Text`
+    font-family: Roboto-Light;
+    font-size:16px;
+    color:#D42026;  
+`;
+
+
+export const BodyList = styled.View`
+    flex:1;
+    background-color:#D42026;
+`;
+
+
+export const TitleList = styled.Text`
+    font-family: Roboto Regular;
+    font-size:16px;
+    color:white;  
+`;
+
